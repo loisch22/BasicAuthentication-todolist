1514375fb397c0fc51a434903ff22703071e1174
loisch22 <loisch22@gmail.com>
2017-10-17 10:11:03 -0700

    add test methods for model and controllers

---
+++ ToDoList.Tests/ControllerTests/ItemsControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using ToDoList.Controllers;
+using System.Collections.Generic;
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class ItemsControllerTest
+    {
+        [TestMethod]
+        public void GetDescriptionTest()
+        {
+            ItemsController controller = new ItemsController();
+
+            var result = controller.Index();
+
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
+
+        [TestMethod]
+        public void Get_ModelList_Index_Test()
+        {
+            ViewResult indexView = new ItemsController().Index() as ViewResult;
+
+            var result = indexView.ViewData.Model;
+
+            Assert.IsInstanceOfType(result, typeof(List<Item>));
+        }
+
+    }
+}
---
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class ItemTest
+    {
+        [TestMethod]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var item = new Item();
+
+            item.Description = "Wash the dog";
+            //Act
+            var result = item.Description;
+
+            //Assert
+            Assert.AreEqual("Wash the dog", result);
+        }
+    }
+}
